8f2b94b8ff812e0b00ccbeb0976eef5f6911a8d5
trabth-0467 <bui.thi.huong.tra@sun-asterisk.com>
2025-03-13 17:01:08 +0700

    Chapter9

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_03_07_184828) do
+ActiveRecord::Schema[7.0].define(version: 2025_03_13_043601) do
   create_table "products", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -19,11 +19,12 @@ ActiveRecord::Schema[7.0].define(version: 2025_03_07_184828) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
-    t.string "email"
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "string"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
