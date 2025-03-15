7eb092508a77f8dc6f4ff768601b35af53a8a0be
trabth-0467 <bui.thi.huong.tra@sun-asterisk.com>
2025-03-16 03:15:32 +0700

    Chapter10

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,10 +8,7 @@ module SessionsHelper
       @current_user ||= User.find_by id: user_id
     elsif user_id = cookies.signed[:user_id]
       user = User.find_by id: user_id
-      if user&.authenticate?(cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
+      @current_user = user if user&.authenticated?(cookies[:remember_token])
     end
   end
 
@@ -35,5 +32,18 @@ module SessionsHelper
     user.forget
     cookies.delete :user_id
     cookies.delete :remember_token
+  end
+
+  def current_user? user
+    user && user == current_user
+  end
+
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
+  def redirect_back_or default
+    redirect_to session[:forwarding_url] || default
+    session.delete :forwarding_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
     delete "logout", to: "sessions#destroy"
     get "signup", to: "users#new"
     post "signup", to: "users#create"
-    resources :users, only: :show
+    resources :users, only: [:show, :edit, :index, :update, :destroy]
   end
 end
